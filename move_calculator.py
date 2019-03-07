a276d643ea6f78680cfc5dedc006e70a7175f213
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2019-03-07 10:43:31 -0500

    improved it to determine chunk size

--- move_calculator.py
+++ move_calculator.py
@@ -26,7 +26,7 @@ def initializations(filename):
                 padded_matrix[row+1].append(num)
             padded_matrix[row+1].append(None)
         padded_matrix.append([None for i in range(collen+2)])
-    return pos, moves, padded_matrix
+    return pos, 50, padded_matrix
 
 def write_to_file(move_list):
     my_dir = os.path.dirname(os.path.realpath(__file__))
@@ -44,16 +44,17 @@ def recursive_step(pos, moves, matrix, total, max, move_list, max_move_list, end
         return max, max_move_list, end_pos
     for row in range(-1, 2):
         for col in range(-1, 2):
-            new_pos = [pos[0]+row, pos[1]+col]
-            try:
-                spot_value = matrix[new_pos[0]][new_pos[1]]
-                matrix[new_pos[0]][new_pos[1]] = 0
-                new_total = total+spot_value
-                new_move_list = move_list + [[row, col]]
-                max, max_move_list, end_pos= recursive_step(new_pos, moves-1, matrix, new_total, max, new_move_list, max_move_list, end_pos)
-                matrix[new_pos[0]][new_pos[1]] = spot_value
-            except:
-                pass
+            if (row!=0 or col!=0):
+                new_pos = [pos[0]+row, pos[1]+col]
+                try:
+                    spot_value = matrix[new_pos[0]][new_pos[1]]
+                    matrix[new_pos[0]][new_pos[1]] = 0
+                    new_total = total+spot_value
+                    new_move_list = move_list + [[row, col]]
+                    max, max_move_list, end_pos= recursive_step(new_pos, moves-1, matrix, new_total, max, new_move_list, max_move_list, end_pos)
+                    matrix[new_pos[0]][new_pos[1]] = spot_value
+                except:
+                    pass
 
     return max, max_move_list, end_pos
 
@@ -69,14 +70,20 @@ def find_moves(filename):
     pos, moves, matrix = initializations(filename)
     pos[0] +=1
     pos[1] +=1
-    chunk_size = 5
+    chunk_size = 6
+    if moves > 50:
+        chunk_size = 5
+        if moves > 300:
+            chunk_size = 4
+            if moves > 2250:
+                chunk_size = 3
     move_list = []
     spot_value = matrix[pos[0]][pos[1]]
     matrix[pos[0]][pos[1]]=0
     max = 0
     for i in range(moves//chunk_size):
         maxes = recursive_step(pos, chunk_size, matrix, spot_value, 0, [], [], pos)
-        print(maxes)
+
         max+=maxes[0]
         matrix = mark_zeros(matrix, pos, maxes[1])
         pos = maxes[2]
@@ -84,9 +91,7 @@ def find_moves(filename):
     # print(maxes)
     [new_max, move_order, _] = recursive_step(pos, moves%chunk_size, matrix, spot_value, 0, [], [], pos)
     max+=new_max
-    print(max)
     move_list += move_order
-    print(move_list)
     write_to_file(move_list)
 
 
